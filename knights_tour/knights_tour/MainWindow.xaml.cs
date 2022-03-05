7a9c532cb77b0c88b5b59560a42f8629a1b27fc3
Paul Arseni Ailoi <arseniailoi002@gmail.com>
2022-03-05 14:24:59 +0200

    added project/solution to begin development

--- knights_tour/knights_tour/MainWindow.xaml.cs
+++ knights_tour/knights_tour/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace knights_tour
+namespace Knights_Tour
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
